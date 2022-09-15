389545f2d6c4a55a3a45d1bddcd259caf574f47b
Sang kwon Yeum <kunshup2000@gmail.com>
2022-09-15 09:18:32 +0900

    feat: env 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_BASE_URL);
   return (
     <div className="App">
       <header className="App-header">
